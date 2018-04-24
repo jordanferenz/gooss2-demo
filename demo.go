8b02aa22f089adc13bfb284a5e7c7e790302d6b7
Jordan H. Ferenz <jordan.ferenz@aquasec.com>
2018-04-24 20:27:29 +0300

    Add files for go dependency management gotcha
    To demonstrate the problem with Go's dependency management, we create a container with Go installed and put there demo code.

---
+++ demo.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jordanferenz/gooss2-demo/light"
+)
+
+func main() {
+	fmt.Printf("Is light a particle or wave? %s\n", light.Observe())
+}
---
+++ light/main.go
@@ -0,0 +1,5 @@
+package main
+
+func Observe() string {
+	return "particle"
+}
